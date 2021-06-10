e7b2f4e32f9d82eae32a01d5097f66fff87b1858
Keigh Rim <keigh.rim@gmail.com>
2021-06-10 17:49:04 -0400

    adding vocabulary package to public documentation

--- documentation/conf.py
+++ documentation/conf.py
@@ -5,7 +5,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import datetime
-import sphinx_rtd_theme
 
 # -- Path setup --------------------------------------------------------------
 
--- mmif/serialize/view.py
+++ mmif/serialize/view.py
@@ -130,8 +130,8 @@ class View(FreezableMmifObject):
         Look for certain annotations in this view, specified by parameters
 
         :param at_type: @type of the annotations to look for. When this is None, any @type will match.
-        :param properties: properties of the annotations to look for. When given more than one property, all properties 
-        must match. Note that annotation type metadata are specified in the `contains` view metadata, not in individual
+        :param properties: properties of the annotations to look for. When given more than one property, all properties \
+        must match. Note that annotation type metadata are specified in the `contains` view metadata, not in individual \
         annotation objects.
         """
         def prop_check(k, v, *props):
