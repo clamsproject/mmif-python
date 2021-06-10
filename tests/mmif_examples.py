55f149e96b7cd4b5a8e8b1306c0042f04e00c88d
Keigh Rim <keigh.rim@gmail.com>
2021-06-10 10:04:31 -0400

    refactored test examples

--- tests/mmif_examples.py
+++ tests/mmif_examples.py
@@ -3,34 +3,29 @@ from urllib import request
 from mmif import __specver__
 from string import Template
 
-__all__ = ['JSON_STR',
-           'MMIF_EXAMPLES',
-           'SUB_EXAMPLES']
-
-
-def substitute(example_dict: dict) -> dict:
-    return dict((k, Template(v).substitute(specver=__specver__)) for k, v in example_dict.items())
-
+__all__ = [
+    'EVERYTHING_JSON',
+    'MMIF_EXAMPLES',
+    'FRACTIONAL_EXAMPLES',
+]
 
 everything_file_url = f"https://raw.githubusercontent.com/clamsproject/mmif/{__specver__}/specifications/samples/everything/raw.json"
 res = request.urlopen(everything_file_url)
-# TODO (krim @ 5/26/21): when https://github.com/clamsproject/mmif/pull/173 is merged, 
-# everything file retrieved from github will have proper version number encoded. 
-# so the following line and "substitute" calls might be necessary later
-JSON_STR = res.read().decode('utf-8').replace('0.3.0', '${specver}')
+EVERYTHING_JSON = res.read().decode('utf-8')
 
-example_templates = dict(
-    mmif_example1=JSON_STR
-)
+MMIF_EXAMPLES = {
+    'everything': Template(EVERYTHING_JSON),
+}
+FRACTIONAL_EXAMPLES = {
+    'doc_only': Template("""{
+"@type": "http://mmif.clams.ai/${VERSION}/vocabulary/TextDocument",
+"properties": {
+"id": "td999",
+"mime": "text/plain",
+"location": "file:///var/archive/transcript-1000.txt" 
+}
+}"""),
+}
 
-sub_example_templates = {'doc_example': """{
-  "@type": "http://mmif.clams.ai/${specver}/vocabulary/TextDocument",
-    "properties": {
-      "id": "td999",
-      "mime": "text/plain",
-      "location": "file:///var/archive/transcript-1000.txt" 
-    }
-}"""}
-
-MMIF_EXAMPLES = substitute(example_templates)
-SUB_EXAMPLES = substitute(sub_example_templates)
+MMIF_EXAMPLES = dict((k, v.substitute(VERSION=__specver__)) for k, v in MMIF_EXAMPLES.items())
+FRACTIONAL_EXAMPLES = dict((k, v.substitute(VERSION=__specver__)) for k, v in FRACTIONAL_EXAMPLES.items())
--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -19,28 +19,28 @@ class TestAnnotationTypes(unittest.TestCase):
         self.assertEqual(string_of_two, string_out)
 
     def test_use_in_mmif(self):
-        mmif_obj = Mmif(MMIF_EXAMPLES['mmif_example1'], frozen=False)
+        mmif_obj = Mmif(MMIF_EXAMPLES['everything'], frozen=False)
         view_obj: View = mmif_obj.get_view_by_id('v1')
         view_obj.new_annotation(AnnotationTypes.Polygon, 'p1')
         view_obj.new_annotation(AnnotationTypes.TimeFrame, 'bb2')
         self.assertEqual(list(view_obj.metadata.contains.keys()), [f'http://mmif.clams.ai/{__specver__}/vocabulary/TimeFrame', f'http://mmif.clams.ai/{__specver__}/vocabulary/Polygon'])
 
     def test_type_checking(self):
-        mmif_obj = Mmif(MMIF_EXAMPLES['mmif_example1'])
+        mmif_obj = Mmif(MMIF_EXAMPLES['everything'])
         ann_obj = mmif_obj.get_view_by_id('v1').annotations['s1']
         self.assertTrue(ann_obj.is_type(ann_obj.at_type))
         self.assertTrue(ann_obj.is_type(str(ann_obj.at_type)))
         self.assertFalse(ann_obj.is_type(DocumentTypes.VideoDocument))
 
     def test_serialize_within_mmif(self):
-        mmif_obj = Mmif(MMIF_EXAMPLES['mmif_example1'], frozen=False)
+        mmif_obj = Mmif(MMIF_EXAMPLES['everything'], frozen=False)
         view_obj = mmif_obj.get_view_by_id('v5')
         view_obj.annotations._items.pop('bb25')
         anno_obj = view_obj.new_annotation(AnnotationTypes.BoundingBox, 'bb25')
         anno_obj.add_property('coordinates', [[150, 810], [1120, 810], [150, 870], [1120, 870]])
         anno_obj.add_property('timePoint', 21000)
         anno_obj.add_property('boxType', 'text')
-        expected = json.loads(Mmif(MMIF_EXAMPLES['mmif_example1']).serialize())
+        expected = json.loads(Mmif(MMIF_EXAMPLES['everything']).serialize())
         actual = json.loads(mmif_obj.serialize())
         bb_type = f'http://mmif.clams.ai/{__specver__}/vocabulary/BoundingBox'
         expected['views'][4]['metadata']['contains'][bb_type]['gen_time'] = 'dummy'
