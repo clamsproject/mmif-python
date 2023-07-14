6b75ebf8bc4566176a6ad44115f2dc0314d2afa8
Keigh Rim <keigh.rim@gmail.com>
2023-07-14 13:14:09 -0400

    replaced deprecated `pkg_resources` with `importlib.resources`

--- mmif/__init__.py
+++ mmif/__init__.py
@@ -1,4 +1,4 @@
-import pkg_resources
+import importlib.resources
 
 # DO NOT CHANGE THIS ORDER, important to prevent circular imports
 from mmif.ver import __version__
@@ -13,7 +13,5 @@ _schema_res_name = 'mmif.json'
 
 
 def get_mmif_json_schema():
-    res = pkg_resources.resource_stream(f'{__name__}.{_res_pkg}', _schema_res_name)
-    res_str = res.read().decode('utf-8')
-    res.close()
-    return res_str
+    # TODO (krim @ 7/14/23): use `as_file` after dropping support for Python 3.8
+    return importlib.resources.read_text(f'{__package__}.{_res_pkg}', _schema_res_name)
