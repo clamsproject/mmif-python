40833f558ca4726c2100892f7d3db7eafb2c7259
Keigh Rim <keigh.rim@gmail.com>
2023-07-14 13:09:13 -0400

    Reverted PR #225
    This reverts commit 6b5fc195c3cd14c562992f11588d48446b52ac9f..53fc487e0e2f18c3c9deb396c8bfd3b5b3212f1a

--- mmif/__init__.py
+++ mmif/__init__.py
@@ -1,9 +1,3 @@
-import importlib
-import pkgutil
-import re
-from collections import defaultdict
-from typing import Dict, Type, Callable, Set
-
 import pkg_resources
 
 # DO NOT CHANGE THIS ORDER, important to prevent circular imports
@@ -23,14 +17,3 @@ def get_mmif_json_schema():
     res_str = res.read().decode('utf-8')
     res.close()
     return res_str
-
-
-patches: Dict[Type, Set[Callable]] = defaultdict(set)
-for _, name, ispkg in pkgutil.iter_modules():
-    if ispkg and re.match(r'mmif[-_]utils[-_]', name):
-        mod = importlib.import_module(name)
-        for c, ms in mod.patches.items():
-            for m in ms:
-                if m in patches[c]:
-                    raise ValueError(f'Patch for {c}::{m.__name__} already exists.')
-                patches[c].add(m)
