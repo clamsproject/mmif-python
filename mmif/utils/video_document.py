658e62bd4fad7a18dd410abae591ef206d5a2eb2
Keigh Rim <keigh.rim@gmail.com>
2023-07-14 14:02:18 -0400

    some refactoring

--- mmif/utils/video_document.py
+++ mmif/utils/video_document.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 import numpy as np
 from PIL import Image
@@ -13,10 +14,15 @@ except ImportError as e:
     raise ImportError(
         f"Optional package {e.name} not found. You might want to install Computer-Vision dependencies by running `pip install mmif-python[cv]`")
 
+FPS_DOCPROP_KEY = 'fps'
+
 
 def capture(vd: Document) -> cv2.VideoCapture:
+    if vd is None or vd.at_type != DocumentTypes.VideoDocument:
+        raise ValueError(f'The document does not exist.')
+    
     v = cv2.VideoCapture(vd.location_path())
-    vd.add_property('fps', v.get(cv2.CAP_PROP_FPS))
+    vd.add_property(FPS_DOCPROP_KEY, v.get(cv2.CAP_PROP_FPS))
     return v
 
 
@@ -24,76 +30,34 @@ def get_framerate(vd: Document) -> float:
     if vd is None or vd.at_type != DocumentTypes.VideoDocument:
         raise ValueError(f'The document does not exist.')
 
-    framerate_keys = ('fps', 'framerate')
-    fps = None
+    framerate_keys = (FPS_DOCPROP_KEY, 'framerate')
     for k in framerate_keys:
         if k in vd:
             fps = vd.get_property(k)
             return fps
-    cap = cv2.VideoCapture(vd.location_path())
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    vd.add_property('fps', fps)
-    return fps
+    capture(vd)
+    return vd.get_property(FPS_DOCPROP_KEY)
 
 
-def extract_frames(video_doc: Document, sample_ratio: int, frame_cutoff: int = None) -> List[np.ndarray]:
+def extract_frames(vd: Document, sample_ratio: int, frames_cutoff: int = math.inf, as_PIL: bool = False) -> List[np.ndarray]:
     video_frames = []
-    video_filename = video_doc.location_path()
-
-    # Open the video file
-    video = cv2.VideoCapture(video_filename)
+    video = capture(vd)
     current_frame = 0
-    while video.isOpened():
+    while video.isOpened() and len(video_frames) < frames_cutoff:
         # Read the current frame
         ret, frame = video.read()
 
         if ret:
-            video_frames.append(frame)
+            video_frames.append(Image.fromarray(frame[:, :, ::-1]) if as_PIL else frame)
         else:
             break
 
         # Skip sampleRatio frames
-        current_frame += sample_ratio
+        current_frame += sample_ratio if sample_ratio is not None else 1
         video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
 
-        if frame_cutoff is not None and len(video_frames) > frame_cutoff - 1:
-            break
-
     # Potentially print some statistics like how many frames extracted, sampleRatio, cutoff
-    print(f'Extracted {len(video_frames)} frames from {video_filename}')
-    return video_frames
-
-
-def extract_pil_images(video_doc: Document, sample_ratio: int = 15, frame_cutoff: int = None) -> List[Image]:
-    video_frames = []
-    video_filename = video_doc.location_path()
-
-    # Open the video file
-    video = cv2.VideoCapture(video_filename)
-    current_frame = 0
-    while video.isOpened():
-        # Read the current frame
-        ret, frame = video.read()
-
-        if ret:
-            # Convert it to a PIL image
-            video_frames.append(Image.fromarray(frame[:, :, ::-1]))
-        else:
-            break
-
-        # Skip sampleRatio frames
-        if sample_ratio is not None:
-            current_frame += sample_ratio
-            video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-        else:
-            current_frame += 1
-            video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-
-        if frame_cutoff is not None and len(video_frames) > frame_cutoff - 1:
-            break
-
-    # Potentially print some statistics like how many frames extracted, sampleRatio, cutoff
-    print(f'Extracted {len(video_frames)} frames from {video_filename}')
+    print(f'Extracted {len(video_frames)} frames from {vd.location}')
     return video_frames
 
 
@@ -129,8 +93,7 @@ def frames_to_seconds(video_doc: Document, frames: int, sample_ratio: int) -> fl
 
 def frames_to_milliseconds(video_doc: Document, frames: int, sample_ratio: int) -> float:
     # Needs to take fps and sample ratio
-    fps = get_framerate(video_doc)
-    return frames / (fps * sample_ratio) * 1000
+    return frames_to_seconds(video_doc, frames, sample_ratio) * 1000
 
 
 def seconds_to_frames(video_doc: Document, seconds: float, sample_ratio: int) -> int:
@@ -139,5 +102,4 @@ def seconds_to_frames(video_doc: Document, seconds: float, sample_ratio: int) ->
 
 
 def milliseconds_to_frames(video_doc: Document, milliseconds: float, sample_ratio: int) -> int:
-    fps = get_framerate(video_doc)
-    return int(milliseconds / 1000 * fps * sample_ratio)
+    return seconds_to_frames(video_doc, milliseconds / 1000, sample_ratio)
