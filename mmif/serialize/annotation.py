f65435183561a92c264eaa31bcf2272b3090388c
Keigh Rim <keigh.rim@gmail.com>
2023-07-17 20:17:19 -0400

    added check for existence in the location_path_resolved

--- mmif/serialize/annotation.py
+++ mmif/serialize/annotation.py
@@ -8,6 +8,7 @@ of a view. For documentation on how views are represented, see
 """
 import importlib
 import itertools
+import os
 import pathlib
 import pkgutil
 import re
@@ -446,7 +447,7 @@ class DocumentProperties(AnnotationProperties):
         warnings.warn('location_path() is deprecated. Use location_path_resolved() instead.', DeprecationWarning)
         return self.location_path_resolved()
     
-    def location_path_resolved(self) -> Optional[str]:
+    def location_path_resolved(self, nonexist_ok=True) -> Optional[str]:
         """
         Retrieves only path name of the document location (hostname is ignored), 
         and then try to resolve the path name in the local file system.
@@ -460,11 +461,15 @@ class DocumentProperties(AnnotationProperties):
             return None
         scheme = self.location_scheme()
         if scheme in ('', 'file'):
-            return urlparse(self.location).path
+            p = urlparse(self.location).path
         elif scheme in discovered_docloc_plugins:
-            return discovered_docloc_plugins[scheme].resolve(self.location)
+            p = discovered_docloc_plugins[scheme].resolve(self.location)
         else:
             raise ValueError(f'Cannot resolve location of scheme "{scheme}". Interested in developing mmif-locdoc-{scheme} plugin? See https://clams.ai/mmif-python/plugins')
+        if not nonexist_ok and not os.path.exists(p):
+            raise FileNotFoundError(f'Cannot find file "{p}"')
+        else:
+            return p
 
     def location_path_literal(self) -> Optional[str]:
         """
