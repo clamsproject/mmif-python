707954c13e85264cc11b8ede01e97d9d4f7d2953
Keigh Rim <keigh.rim@gmail.com>
2024-07-01 13:54:41 -0400

    got rid of clams app stuff from migration (#289)

--- mmif/utils/image_document_helper.py
+++ mmif/utils/image_document_helper.py
@@ -1,51 +1,11 @@
-"""
-This app takes in a mmif which has been annotated with bounding boxes. 
-For each timepoint that has a bounding box, the app produces a larger bounding box
-using the outermost coordinates of the initial set. This produces a "concatenation"
-effect, wherein the box is guaranteed to contain every other bounding box. This
-is a useful bit of processing for downstream tasks.
-"""
-# ====================================|
-# Import Statements
-import argparse
-import logging
 from collections import defaultdict
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List
 
-# Imports needed for Clams and MMIF.
-# Non-NLP Clams applications will require AnnotationTypes
+from mmif import AnnotationTypes
 
-from clams import ClamsApp, Restifier
-from mmif import Mmif, View, Annotation, Document, AnnotationTypes, DocumentTypes
-
-# For an NLP tool we need to import the LAPPS vocabulary items
-from lapps.discriminators import Uri
-# ====================================|
-
-class BoundingboxConcatenation(ClamsApp):
-    def __init__(self):
-        super().__init__()
-
-    def _appmetadata(self):
-        # see `metadata.py`
-        pass
-
-    def _annotate(self, mmif: Union[str, dict, Mmif], **parameters) -> Mmif:
-        
-        # see https://sdk.clams.ai/autodoc/clams.app.html#clams.app.ClamsApp._annotate
-
-        # initialize mmif view
-        if not isinstance(mmif, Mmif):
-            mmif_obj = Mmif(mmif)
-        else:
-            mmif_obj = mmif
-        new_view = mmif_obj.new_view()
-        self.sign_view(new_view, parameters)
-        new_view.new_contain(
-            AnnotationTypes.BoundingBox
-        ) 
-
-        # get bounding-box alignment
+if __name__ == '__main__':
+    
+    def read_small_boxes(mmif_obj, **parameters):
         alignment_annotations = mmif_obj.get_alignments(
             at_type1=AnnotationTypes.TimePoint, at_type2=AnnotationTypes.BoundingBox
         )
@@ -85,14 +45,13 @@ class BoundingboxConcatenation(ClamsApp):
                         box_dict[timepoint_anno].append(box_anno)
 
         # concatenate bounding boxes and add to mmif
-        out_coords = self.make_boxes(box_dict)
-        mmif_obj = self.annotate_boxes(mmif_obj, new_view, out_coords, **parameters)
-
+        out_coords = make_big_boxes(box_dict)
         
         return mmif_obj
 
+
     @staticmethod
-    def make_boxes(boundingbox_dictionary) -> Dict[float, List[List[float]]]:
+    def make_big_boxes(boundingbox_dictionary) -> Dict[float, List[List[float]]]:
         """Perform bounding-box concatenation
 
         ### params 
@@ -138,63 +97,3 @@ class BoundingboxConcatenation(ClamsApp):
             c, d = current_max_x, current_max_y
             out[timepoint] = [[a, b], [c, b], [a, d], [c, d]]
         return out
-
-    @staticmethod
-    def annotate_boxes(mmif: Mmif, 
-                       view: View, 
-                       coords: Dict, 
-                       **config) -> Mmif:
-        """Perform mmif annotation with new coordinates.
-        
-        ### params
-        + mmif   => Mmif object we are annotating
-        + view   => View to which we are adding annotations
-        + coords => Dict of individual bounding boxes for each timepoint
-
-        ### returns
-        + Mmif object with new view annotations
-        """
-        for time_point, box_coords in coords.items():
-            bb_annotation = view.new_annotation(AnnotationTypes.BoundingBox)
-            tp_annotation = view.new_annotation(AnnotationTypes.TimePoint)
-            tp_annotation.add_property("timeUnit", config["timeUnit"])
-            tp_annotation.add_property("timePoint", time_point)
-
-            bb_annotation.add_property("label", config["label"])
-
-            bb_annotation.add_property("coordinates", box_coords)
-
-            alignment_annotation = view.new_annotation(AnnotationTypes.Alignment)
-            alignment_annotation.add_property("source", tp_annotation.id)
-            alignment_annotation.add_property("target", bb_annotation.id)
-
-        return mmif
-
-
-def get_app():
-    """
-    This function effectively creates an instance of the app class, without any arguments passed in, meaning, any 
-    external information such as initial app configuration should be set without using function arguments. The easiest
-    way to do this is to set global variables before calling this. 
-    """
-    return BoundingboxConcatenation()
-    
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", action="store", default="5000", help="set port to listen")
-    parser.add_argument("--production", action="store_true", help="run gunicorn server")
-
-    parsed_args = parser.parse_args()
-
-    # create the app instance
-    app = get_app()
-
-    http_app = Restifier(app, port=int(parsed_args.port))
-    # for running the application in production mode
-    if parsed_args.production:
-        http_app.serve_production()
-    # development mode
-    else:
-        app.logger.setLevel(logging.DEBUG)
-        http_app.run()
