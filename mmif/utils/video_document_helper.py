52762c0929ed46d5113dcf228467131c1a7c736a
Keigh Rim <keigh.rim@gmail.com>
2025-02-26 18:31:55 -0500

    vdh frame extraction now redirects ffmpeg errors for downstream

--- mmif/utils/video_document_helper.py
+++ mmif/utils/video_document_helper.py
@@ -1,15 +1,16 @@
 import importlib
 import math
 import warnings
+from io import StringIO
 from typing import Iterable  # todo: replace with collections.abc.Iterable in Python 3.9
-from typing import List, Union, Tuple, Iterator
+from typing import List, Union, Tuple
 
 import mmif
 from mmif import Annotation, Document, Mmif
 from mmif.utils.timeunit_helper import convert
 from mmif.vocabulary import DocumentTypes
 
-for cv_dep in ('cv2', 'ffmpeg', 'PIL'):
+for cv_dep in ('cv2', 'ffmpeg', 'PIL', 'wurlitzer'):
     try:
         importlib.__import__(cv_dep)
     except ImportError as e:
@@ -64,11 +65,13 @@ def get_framerate(video_document: Document) -> float:
         if k in video_document:
             fps = round(video_document.get_property(k), 2)
             return fps
-    capture(video_document)
-    return video_document.get_property(FPS_DOCPROP_KEY)
+    cap = capture(video_document)
+    fps = video_document.get_property(FPS_DOCPROP_KEY)
+    cap.release()
+    return fps
 
 
-def extract_frames_as_images(video_document: Document, framenums: Iterable[int], as_PIL: bool = False):
+def extract_frames_as_images(video_document: Document, framenums: Iterable[int], as_PIL: bool = False, record_ffmpeg_errors: bool = False):
     """
     Extracts frames from a video document as a list of :py:class:`numpy.ndarray`.
     Use with :py:func:`sample_frames` function to get the list of frame numbers first. 
@@ -86,19 +89,27 @@ def extract_frames_as_images(video_document: Document, framenums: Iterable[int],
     tot_fcount = video_document.get_property(FRAMECOUNT_DOCPROP_KEY)
     framenumi = iter(framenums)  # make sure that it's actually an iterator, in case a list is passed
     next_target_f = next(framenumi, None)
-    while True:
-        if next_target_f is None or cur_f > tot_fcount:
-            break
-        ret, frame = video.read()
-        if cur_f == next_target_f:
-            if not ret:
-                sec = convert(cur_f, 'f', 's', video_document.get_property(FPS_DOCPROP_KEY))
-                warnings.warn(f'Frame #{cur_f} ({sec}s) could not be read from the video {video_document.id}.')
-                cur_f += 1
-                continue
-            frames.append(Image.fromarray(frame[:, :, ::-1]) if as_PIL else frame)
-            next_target_f = next(framenumi, None)
-        cur_f += 1
+    from wurlitzer import pipes as cpipes
+    ffmpeg_outs = StringIO()
+    ffmpeg_errs = StringIO()
+    with cpipes(stderr=ffmpeg_errs, stdout=ffmpeg_outs):
+        while True:
+            if next_target_f is None or cur_f > tot_fcount:
+                break
+            ret, frame = video.read()
+            if cur_f == next_target_f:
+                if not ret:
+                    sec = convert(cur_f, 'f', 's', video_document.get_property(FPS_DOCPROP_KEY))
+                    warnings.warn(f'Frame #{cur_f} ({sec}s) could not be read from the video {video_document.id} @ {video_document.location} .')
+                    cur_f += 1
+                    continue
+                frames.append(Image.fromarray(frame[:, :, ::-1]) if as_PIL else frame)
+                next_target_f = next(framenumi, None)
+            cur_f += 1
+    ffmpeg_err_str = ffmpeg_errs.getvalue()
+    if ffmpeg_err_str and record_ffmpeg_errors:
+        warnings.warn(f'FFmpeg output during extracting frames: {ffmpeg_err_str}')
+    video.release()
     return frames
 
 
